55edb60fb1653f1ae22d591b46a9bf2145e1b5fa
mohammedsayed404 <mmossyed525@gmail.com>
2024-03-05 19:36:52 +0200

    modified title of register,login and modified nav-bar and  make background to all app

--- src/app/Views/nav-auth/nav-auth.component.ts
+++ src/app/Views/nav-auth/nav-auth.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-auth',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-auth.component.scss']
 })
 export class NavAuthComponent {
+
+  @HostListener('window:scroll', ['$event'])
+onWindowScroll():void {
+  let element = document.querySelector('.navbar') as HTMLElement;
+  // console.log(window.scrollY);
+// using scrollY because pageYOffset is deprecated
+  if (window.scrollY  > element.clientHeight) {
+    // element.classList.add('bg-info');
+    element.classList.remove('py-4');
+    element.classList.add('fixed-top');
+  } else {
+    // element.classList.remove('bg-info');
+    element.classList.add('py-4');
+    element.classList.remove('fixed-top');
+  }
+}
 
 }
